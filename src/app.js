f48ad5b813dfd3674ac3cebaa8a6a96379ed1355
EQ <167933418+ejquast@users.noreply.github.com>
2024-06-06 01:00:38 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".us", ".io"];
+
+const bios = [];
+
+for (let index = 0; index < pronoun.length; index++) {
+  for (let secondIndex = 0; secondIndex < adj.length; secondIndex++) {
+    for (let thirdIndex = 0; thirdIndex < noun.length; thirdIndex++) {
+      for (let fourthIndex = 0; fourthIndex < extension.length; fourthIndex++) {
+        bios.push(
+          `${pronoun[index]}${adj[secondIndex]}${noun[thirdIndex]}${extension[fourthIndex]}`
+        );
+      }
+    }
+  }
+}
+
+for (const bio of bios) {
+  console.log(bio);
+}
