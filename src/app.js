8fb1206e4fb8db1d694558f18b51548b99ffd6ac
EQ <167933418+ejquast@users.noreply.github.com>
2024-06-11 00:56:21 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -5,20 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let extension = [".com", ".net", ".us", ".io"];
+const pronouns = ["the", "our"];
+const adjectives = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+const extensions = [".com", ".net", ".us", ".io"];
 
 const bios = [];
 
-for (let index = 0; index < pronoun.length; index++) {
-  for (let secondIndex = 0; secondIndex < adj.length; secondIndex++) {
-    for (let thirdIndex = 0; thirdIndex < noun.length; thirdIndex++) {
-      for (let fourthIndex = 0; fourthIndex < extension.length; fourthIndex++) {
-        bios.push(
-          `${pronoun[index]}${adj[secondIndex]}${noun[thirdIndex]}${extension[fourthIndex]}`
-        );
+for (const pronoun of pronouns) {
+  for (const adjective of adjectives) {
+    for (const noun of nouns) {
+      for (const extension of extensions) {
+        bios.push(`${pronoun}${adjective}${noun}${extension}`);
       }
     }
   }
